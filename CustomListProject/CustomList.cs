71fb2f1a6c5c2e7d758dd34d2e6f49a2165b075d
Jake Gambino <jake.gambino1@gmail.com>
2019-09-11 15:19:13 -0500

    Passing test for add to array at index zero, and add to array at next index

---
+++ CustomListProject/CustomList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class CustomList<T> 
+    {
+        public T[] listClass;
+        T[] temporaryCustomList;
+        // member variables
+        public int ItemCount;
+        public int Capacity;
+        public int IndexPosition;
+
+        public T this[int i]
+        {
+            get { return listClass[i]; }
+            set { listClass[i] = value; }
+        }
+
+        // constructor
+        public CustomList()
+        {
+            ItemCount = 1;
+            IndexPosition = 0;
+            Capacity = 2;
+            listClass = new T[Capacity];
+        }
+
+        // member methods
+        public void Add(T value)
+        {
+            AddValueToArray(value);
+            temporaryCustomList = DoubleCapacityOfArrayIfNecessary();
+            CopyOldArrayToNew(listClass, temporaryCustomList);
+            IncrementArray();
+        }
+        public void AddValueToArray(T value)
+        { 
+            listClass[IndexPosition] = value;
+
+            if (ItemCount == Capacity)
+            {
+                DoubleCapacityOfArrayIfNecessary();
+            }
+        }
+        public T[] DoubleCapacityOfArrayIfNecessary()
+        {
+            return temporaryCustomList = new T[Capacity * 2];
+        }
+
+        public T[] CopyOldArrayToNew(T[] listClass, T[] TemporaryCustomList)
+        {
+            for (int i = 0; i < ItemCount; i++)
+            {
+                TemporaryCustomList[i] = listClass[i];
+                listClass = TemporaryCustomList;
+            }
+            return listClass;
+        }
+
+        public void IncrementArray()
+        {
+            IndexPosition++;
+            ItemCount++;
+        }
+
+        public void Remove(T value)
+        {
+
+        }
+    }
+}
--- CustomListUnitTests/UnitTest1.cs
+++ CustomListUnitTests/UnitTest1.cs
@@ -1,14 +1,101 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using CustomListProject;
 namespace CustomListUnitTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddItemToEmptyList_ItemGoesToIndexZero()
         {
+            // arrange
+            CustomList<string> testList = new CustomList<string>();
+            string expected = "Murdock";
+            string actual;
+
+            // act
+            testList.Add("Murdock");
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemToEmptyList_CountIncrementsByOne()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(82);
+            actual = testList.IndexPosition;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddMultipleItemsToList_CountIncrementsByOne()
+        {
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            testList.Add(1);
+            testList.Add(2);
+            actual = testList.ItemCount;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemsToList_DoubleCapacityOfArray()
+        {
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 4;
+            int actual;
+
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+           
+            actual = testList.Capacity;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemsToFullArray_IndicesCopiedOver()
+        {
+            CustomList<int> testList = new CustomList<int>();
+
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+
+            int expected0 = 0;
+            int expected1 = 1;
+            int expected2 = 2;
+            int expected3 = 3;
+            int expected4 = 4;
+
+            int actual0 = testList[0];
+            int actual1 = testList[1];
+            int actual2 = testList[2];
+            int actual3 = testList[3];
+            int actual4 = testList[4];
+
+            Assert.AreEqual(actual0, expected0);
+            Assert.AreEqual(actual1, expected1);
+            Assert.AreEqual(actual2, expected2);
+            Assert.AreEqual(actual3, expected3);
+            Assert.AreEqual(actual4, expected4);
         }
     }
 }
